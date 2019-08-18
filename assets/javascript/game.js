f19ccad1e00a1c21bda0e3d9f577b466c749896a
mclark1990 <mjc8875@gmail.com>
2019-08-18 17:08:49 -0400

    restructured initial start-game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,75 @@
 
 
 //global variables
-let numGuess = 15;
-let wins = 0;
-let lettersGuessed = [];
+
+//game options
 let words = ['scranton', 'beets', 'schrute', 'mifflin', 'dunder'];
 
+//chosen word
+let chosenWord = "";
 
-//generates random number based on length of words array
-const randomNum = Math.floor(Math.random() * words.length);
+//breaks chosen word into letters
+let lettersChosenWord = [];
 
-//stores the random element from words array
-let chosenWord = words[randomNum];
+//number of underscores based on chosen word
+let numBlanks = 0;
 
-// console.log(chosenWord.length);
+//for progress word during game, underscores and letters
+let blanksAndCorrect = [];
 
-//empty array to store word for the game
-let progressWord = [];
+//for letters already guessed
+let alreadyGuessed = [];
+
+//game counters
+let wins = 0;
+let losses = 0;
+let guesses = 10;
+
+//function to start and reset game
+const startGame = () => {
+    
+//reset guesses    
+guesses = 10;
+    
+//randomly choose from words array
+chosenWord = words[Math.floor(Math.random() * words.length)];
+
+//breaks into letters
+lettersChosenWord = chosenWord.split("");
+
+//sets to length of chosen word
+numBlanks = lettersChosenWord.length;
+
+console.log(chosenWord);
+console.log(lettersChosenWord);
+console.log(numBlanks);
+
+//resets arrays
+blanksAndCorrect = [];
+alreadyGuessed = [];
 
 
-//function to generate the underscores
-let createUnderscore = () => {
 
-    for (let i = 0; i < chosenWord.length; i++ ){
-        progressWord.push('_ ')
-        progressWord.toString()
-        document.getElementById('current').innerHTML = progressWord.join(" ")
- 
-    }
+
+
+
+//loop to add underscores for the chosen word
+for (let i = 0; i < numBlanks; i++){
+    blanksAndCorrect.push("_");
+}
+console.log(blanksAndCorrect);
+
+//resets guesses to 10
+document.getElementById('remaining-guess').innerHTML = guesses;
+
+//pushes underscores to the current word
+document.getElementById('current').innerHTML = blanksAndCorrect.join(" ");
+
+//resets letters already guessed blank
+document.getElementById('already-guessed').innerHTML = alreadyGuessed.join(" ")
+
 
 }
-console.log(progressWord);
 
-//initial number of guesses to DOM
-document.getElementById('remaining-guess').innerHTML = numGuess;
-//initial wins to DOM
-document.getElementById('wins').innerHTML = "Wins: " + wins;
 
-//call for creating the underscore
-createUnderscore();
+startGame();
